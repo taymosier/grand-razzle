172fd476ebe7a6a1261a297d029cdb11c8ba3771
Taylor Mosier <taymosier@gmail.com>
2019-07-16 01:11:04 -0400

    additional styling for billboards and flavor text

--- src/Components/FlavorCard/FlavorCard.js
+++ src/Components/FlavorCard/FlavorCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Col, Row } from 'reactstrap';
 import './flavorCard.css';
 
 export class FlavorCard extends Component {
@@ -12,11 +13,33 @@ export class FlavorCard extends Component {
   //splitText here
 
   render(){
+    console.log(typeof this.state.flavor)
     return(
       <div className={"flavor-card"}>
-        <div className="card-filter" />
-        <p className={"flavor-text"}>{this.state.flavor}</p>
+        <div className="contents">
+          <div className="card-filter" />
+          <Col
+            className="text-container"
+            xl={{ size: 12, offset: 0 }}
+            lg={{ size: 12, offset: 0 }}
+            md={{ size: 12, offset: 0 }}
+            sm={{ size: 12, offset: 0 }}
+            xs={{ size: 12, offset: 0 }}
+          >
+          {this.state.flavor !== undefined && typeof this.state.flavor === "object"
+            ? this.state.flavor.map((block) => {
+               return (
+                <p className="flavor-text">
+                  {block}
+                </p>
+              )
+            })
+            : <p className="flavor-text">{this.state.flavor}</p>
+          }
+          </Col>
+        </div>
       </div>
+
     )
   }
 }
